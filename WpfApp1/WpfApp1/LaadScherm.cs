3247c33b9cacc0a436a14e514a36247a6d5a74ff
Jacsquare <43779942+Jacsquare@users.noreply.github.com>
2018-10-10 14:19:03 +0200

    Add files via upload
    Scherm om het spel te laden

---
+++ WpfApp1/WpfApp1/LaadScherm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+
+namespace WpfApp1
+{
+
+    public class LaadSchermGrid
+    {
+        private Grid grid;
+
+        public LaadSchermGrid(Grid grid, int kolommen, int rijen)
+        {
+            this.grid = grid;
+            InitializeLaadScherm(kolommen, rijen);
+
+            AddTitle();
+            AddTable();
+        }
+
+        private void InitializeLaadScherm(int kolommen, int rijen)
+        {
+            for (int i = 0; i < rijen; i++)
+            {
+                grid.RowDefinitions.Add(new RowDefinition());
+            }
+            for (int i = 0; i < kolommen; i++)
+            {
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+        }
+        private void AddTitle()
+        {
+            Label title = new Label();
+            title.Content = "Load your game";
+            title.FontSize = 40;
+            title.HorizontalAlignment = HorizontalAlignment.Center;
+
+            Grid.SetRow(title, 0);
+            Grid.SetColumn(title, 0);
+            Grid.SetColumnSpan(title, 20);
+            Grid.SetRowSpan(title, 3);
+            grid.Children.Add(title);
+    }
+
+        private void AddTable()
+        {
+            Label Naam = new Label();
+            Naam.Content = "Naam";
+            Naam.FontSize = 20;
+            Naam.HorizontalAlignment = HorizontalAlignment.Center;
+        }
+        private void TableGrid()
+        {
+            Border TableGrid = new Border();
+            TableGrid.Height = 30;
+            TableGrid.Width = 30;
+        }
+    }
+}
