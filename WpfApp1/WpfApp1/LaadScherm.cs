59f95bf669b587a5af02ff6259ffc20fd77df61f
Jacsquare <43779942+Jacsquare@users.noreply.github.com>
2018-10-11 11:43:45 +0200

    Update #2

--- WpfApp1/WpfApp1/LaadScherm.cs
+++ WpfApp1/WpfApp1/LaadScherm.cs
@@ -44,22 +44,58 @@ namespace WpfApp1
             Grid.SetRow(title, 0);
             Grid.SetColumn(title, 0);
             Grid.SetColumnSpan(title, 20);
-            Grid.SetRowSpan(title, 3);
+            Grid.SetRowSpan(title, 2);
             grid.Children.Add(title);
     }
 
         private void AddTable()
         {
+            /// https://docs.microsoft.com/en-us/dotnet/api/system.windows.controls.grid?view=netframework-4.7.2
+            /// Naam
             Label Naam = new Label();
             Naam.Content = "Naam";
-            Naam.FontSize = 20;
+            Naam.FontSize = 15;
             Naam.HorizontalAlignment = HorizontalAlignment.Center;
-        }
-        private void TableGrid()
-        {
-            Border TableGrid = new Border();
-            TableGrid.Height = 30;
-            TableGrid.Width = 30;
+
+            Grid.SetRow(Naam,5);
+            Grid.SetColumn(Naam, 6);
+            Grid.SetColumnSpan(Naam, 2);
+            Grid.SetRowSpan(Naam, 2);
+            grid.Children.Add(Naam);
+            /// Gemaakt door
+            Label Gemaaktdoor = new Label();
+            Gemaaktdoor.Content = "Gemaakt door";
+            Gemaaktdoor.FontSize = 15;
+            Gemaaktdoor.HorizontalAlignment = HorizontalAlignment.Center;
+
+            Grid.SetRow(Gemaaktdoor, 5);
+            Grid.SetColumn(Gemaaktdoor, 8);
+            Grid.SetColumnSpan(Gemaaktdoor, 2);
+            Grid.SetRowSpan(Gemaaktdoor, 2);
+            grid.Children.Add(Gemaaktdoor);
+
+            /// Score
+            Label score = new Label();
+            score.Content = "Score";
+            score.FontSize = 15;
+            score.HorizontalAlignment = HorizontalAlignment.Center;
+
+            Grid.SetRow(score, 5);
+            Grid.SetColumn(score, 10);
+            Grid.SetColumnSpan(score, 2);
+            Grid.SetRowSpan(score, 2);
+            grid.Children.Add(score);
+            /// Datum
+            Label Datum = new Label();
+            Datum.Content = "Datum";
+            Datum.FontSize = 15;
+            Datum.HorizontalAlignment = HorizontalAlignment.Center;
+
+            Grid.SetRow(Datum, 5);
+            Grid.SetColumn(Datum, 12);
+            Grid.SetColumnSpan(Datum, 2);
+            Grid.SetRowSpan(Datum, 2);
+            grid.Children.Add(Datum);
         }
     }
 }
