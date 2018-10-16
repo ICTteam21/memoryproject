bd22074cc15c85b3e68f7b68754df3bc0de34bc2
Maurice Ponte <maurice.leon.ponte@gmail.com>
2018-10-16 12:35:42 +0200

    eindelijk goed

---
+++ MemoryGame/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace MemoryGame
+{
+
+    public partial class MainMenuWindow : Window
+    {
+
+        MainClass grid;
+
+        public MainMenuWindow()
+        {
+            InitializeComponent();
+
+            grid = new MainClass(this, StartScherm);
+
+
+        }
+
+    }
+
+}
+
